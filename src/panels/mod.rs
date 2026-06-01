414eb1e801a1613cde9a0829933719f9d2e3450e
Makhi Burroughs <nger10449@gmail.com>
2026-06-01 02:18:35 -0400

    Delete src directory

--- src/panels/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod chat;
-pub mod hub;
-pub mod inspector;
-pub mod memory;
-pub mod settings;
-pub mod training;
--- src/widgets/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod heatmap;
-pub mod message_bubble;
-pub mod metric_card;
-pub mod status_bar;
